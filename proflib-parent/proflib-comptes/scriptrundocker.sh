085fe952a4a5406140b4c8bd3d3b6c4952e340ba
laurent <kanailaurent@gmail.com>
2021-12-29 22:28:33 +0100

    demarre conf ansible

--- proflib-parent/proflib-comptes/scriptrundocker.sh
+++ proflib-parent/proflib-comptes/scriptrundocker.sh
@@ -1,3 +1,4 @@
+ansible-playbook -i ansible/env/local/00_inventory.yml -C ansible/playbook.yml
 #docker run -d --name=proflibcomptesdb  -p3306:3306 -e "MYSQL_ROOT_PASSWORD=chenlaouche" -e "MYSQL_DATABASE proflibcomptesdb" mariadb:latest
 $M3/mvn clean
 $M3/mvn package
