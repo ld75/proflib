5dad2f606f6375b2de252674216d6ce12cd1d845
laurent <kanailaurent@gmail.com>
2022-01-09 13:44:21 +0100

    live-server ready

--- proflib-parent/proflib-front/src/main/web-app/js/CarteDeVisite.js
+++ proflib-parent/proflib-front/src/main/web-app/js/CarteDeVisite.js
@@ -1,5 +1,12 @@
 export class CarteDeVisite extends HTMLElement{
+constructor()
+    {
+        super();
+        let div = document.createElement("div")
+        div.innerHTML="haha"
+        this.appendChild(div);
 
+    }
 }
 window.customElements.define('proflib-cartedevisite', CarteDeVisite);
 
