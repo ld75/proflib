bbae30a220419c0e927b95981052761443aa01cc
lduverge <kanailaurent@gmail.com>
2022-02-13 14:44:31 +0100

    Preparation demo github

--- proflib-parent/proflib-front/src/main/web-app/js/DureeDeplacementRDV.js
+++ proflib-parent/proflib-front/src/main/web-app/js/DureeDeplacementRDV.js
@@ -1,9 +1,8 @@
 export class DureeDeplacementRDV
 {
-calculDureeDeplacement(vitesseKmh, distanceKm){
-if (vitesseKmh<0 || distanceKm<0)throw Error("illegal args")
-if (vitesseKmh==0)throw Error("infini")
-return distanceKm*60/vitesseKmh;
-}
-
+    calculDureeDeplacement(vitesseKmh, distanceKm){
+    if (vitesseKmh<0 || distanceKm<0)throw Error("illegal args")
+    if (vitesseKmh==0)throw Error("infini")
+    return distanceKm*60/vitesseKmh;
+    }
 }
